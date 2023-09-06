4a22d93c557cd6edf9fb0812185133a0cc86bf48
stripy1026 <aequatio1026@gmail.com>
2023-09-06 21:04:36 +0900

    feat: :sparkles: widget init, eslint inline rule add

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   root: true,
   extends: '@react-native',
   rules: {
+    'react-native/no-inline-styles': 0,
     'prettier/prettier': [
       'error',
       {
         endOfLine: 'auto',
+        'no-inline-styles': false,
       },
     ],
   },
