05864fa647e24e96d846fe1ec7e392b3abb3a4f1
stripy1026 <aequatio1026@gmail.com>
2023-09-06 22:05:28 +0900

    feat: :sparkles: add widget task handler
    need to get android directory

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-import App from './App';
+import {registerWidgetTaskHandler} from 'react-native-android-widget';
 import {name as appName} from './app.json';
+import App from './App';
+import {widgetTaskHandler} from '@/components/widget-task-handler';
 
 AppRegistry.registerComponent(appName, () => App);
+registerWidgetTaskHandler(widgetTaskHandler);
