1206ab2e08eafc6f37543906576cd2bcb0abea50
stripy1026 <aequatio1026@gmail.com>
2023-09-08 03:26:30 +0900

    test: :sparkles: update widget preview screen

--- src/screens/HelloWidgetPreviewScreen.tsx
+++ src/screens/HelloWidgetPreviewScreen.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
-import {ImageBackground, StyleSheet, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {WidgetPreview} from 'react-native-android-widget';
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
 import {HelloWidget} from '@/widgets/HelloWidget';
-import {STORAGE_KEY} from '@/store/store';
 
-export async function HelloWidgetPreviewScreen() {
-  const data =
-    (await AsyncStorage.getItem(STORAGE_KEY)) ??
-    '데이터를 불러오지 못했습니다.';
-
+export function HelloWidgetPreviewScreen() {
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={require('@/assets/temp_background.jpeg')}
-        resizeMode="cover">
-        <WidgetPreview
-          renderWidget={() => <HelloWidget text={data} />}
-          width={320}
-          height={200}
-        />
-      </ImageBackground>
+      <WidgetPreview
+        renderWidget={() => <HelloWidget text="Hello" />}
+        width={320}
+        height={200}
+      />
     </View>
   );
 }
